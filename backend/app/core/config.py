2302114b705a0a2ead8d1739756b1fa8edc36d88
sadumina <it23394124@my.sliit.lk>
2026-02-24 12:04:38 +0530

    fix some erros in backend

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,5 +6,6 @@ load_dotenv()
 class Settings:
     PROJECT_NAME = "EduSense"
     DATABASE_URL = os.getenv("DATABASE_URL")
+    DATABASE_NAME = os.getenv("DATABASE_NAME", "edusense_db")
 
 settings = Settings()
