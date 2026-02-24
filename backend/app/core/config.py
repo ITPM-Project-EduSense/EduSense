6a707e28a7d9cd014e8fd14c75073a647a65ec31
sadumina <it23394124@my.sliit.lk>
2026-02-24 15:53:18 +0530

    update config file

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -7,5 +7,6 @@ class Settings:
     PROJECT_NAME = "EduSense"
     DATABASE_URL = os.getenv("DATABASE_URL")
     DATABASE_NAME = os.getenv("DATABASE_NAME", "edusense_db")
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 settings = Settings()
